a1c3a5c81b02c4d5b57ca2e30edd55b27c6c1386
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-02-26 22:51:05 +0500

    change def validate. add checking patch and put.

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -40,8 +40,16 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
         request_method = self.context['request'].method
         creator = self.context['request'].user
+        open_adv_count = Advertisement.objects.filter(creator=creator).filter(status='OPEN').count()
 
-        if request_method == 'POST':
-            if Advertisement.objects.filter(creator=creator).filter(status='OPEN').count() >= 10:
-                raise ValidationError('У вас не может быть более 10 открытых запросов.')
+        if request_method == 'POST' and open_adv_count >= 10:
+            raise ValidationError('У вас не может быть более 10 открытых запросов.')
+
+        elif request_method in ['PATCH', 'PUT']:
+            new_status = data.get('status')
+            if new_status == 'OPEN':
+                current_status = self.instance.status
+                if current_status == 'CLOSED' and open_adv_count >= 10:
+                    raise ValidationError('Объявление не открыть - у вас не может быть более 10 открытых объявлений.')
         return data
+
